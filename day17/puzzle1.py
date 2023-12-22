9a71710538dd3a4d095727f216a9c84683e4b60f
Piotr <piort.wiercigroch1@gmail.com>
2023-12-22 18:15:09 +0100

    day 17 done

--- day17/puzzle1.py
+++ day17/puzzle1.py
@@ -8,87 +8,123 @@ inputf.close()
 import heapq as hq
 from collections import defaultdict
 
+# length vector
+d = defaultdict(lambda : float('inf'))
+p = {}
+
+# visited vector
+visited = defaultdict(lambda : False)
+
 # DIRECTIONS ENUM
 UP = 1
 DOWN = 2
 RIGHT = 3
 LEFT = 4
 DIRECTION_TO_CORDS_SHIFT = {UP: (-1, 0), DOWN: (1, 0), RIGHT: (0, 1), LEFT: (0, -1)}
+BACK_STEP = {UP: DOWN, DOWN: UP, RIGHT: LEFT, LEFT: RIGHT}
 
+def get_neighbors(u):
+    for DIR in [UP, DOWN, RIGHT, LEFT]:
+        # If the neighbor is not a step back
+        if DIR != BACK_STEP[u[2]]:
+            shift = DIRECTION_TO_CORDS_SHIFT[DIR]
+            new_blocks_count = 1 if DIR != u[2] else u[3] + 1
+            cords_x = u[0] + shift[0]
+            cords_y = u[1] + shift[1]
+            if 0 <= cords_x < len(input) and 0 <= cords_y < len(input[0]) and new_blocks_count <= 3:
+                yield (cords_x, cords_y, DIR, new_blocks_count)
 
-# LENGTH VECTOR DEFINITION
-d = defaultdict(lambda : float('inf'))
+def find_index_by_values_filter(lst, target_values):
+    filtered_tuples = filter(lambda tpl: tpl[1:5] == target_values, lst)
+    try:
+        index = lst.index(next(filtered_tuples))
+    except StopIteration:
+        index = -1
+    return index
 
-# VISITED VECTOR INIT
-V = defaultdict(lambda : False)
+def get_ans():
+    res = []
+    for i in range(1, 5):
+        for j in range(3):
+            ans = d[(len(input)-1, len(input[0])-1, i, j)]
+            if ans != float('inf'):
+                res.append(ans)
+    return res
 
-def get_key(coordinates, direction, blocks_in_line):
-    return coordinates[0], coordinates[1], direction, blocks_in_line
+def print_lens():
+    for x in range(len(input)):
+        for y in range(len(input[0])):
+            res = []
+            for i in range(1, 5):
+                for j in range(3):
+                    ans = d[(x, y, i, j)]
+                    if ans != float('inf'):
+                        res.append(ans)
+            print(min(res), end=" ")
+        print()
 
-def key_to_val(key):
-    return (key[0], key[1]), key[2], key[3]
+def print_path():
 
-def dput(key, new_d):
-    d[key] = new_d
+    DIR_TO_CHAR = {UP: "^", DOWN: "v", RIGHT: ">", LEFT: "<"}
 
-def dget(key):
-    return d[key]
+    best = float('inf')
+    bc = None
+    for i in range(1, 5):
+        for j in range(3):
+            ans = d[(len(input)-1, len(input[0])-1, i, j)]
+            if ans < best:
+                bc = (len(input)-1, len(input[0])-1, i, j)
+                best = ans
+    
+    node = bc
+    ic = input.copy()
+    ic = list(map(list, ic))
+    while True:
+        ic[node[0]][node[1]] = DIR_TO_CHAR[node[2]]
+        node = p[node]
+        if node[0] == 0 and node[1] == 0:
+            break
+    for row in ic:
+        for c in row:
+            print(c, end="")
+        print()
 
-def vput(key):
-    V[key] = True
+# key struct
+# tuple(x coordinate, y coordinate, direction, blocks count)
+start_key_arr = [(0, 0, RIGHT, 0), (0, 0, DOWN, 0)]
 
-def vget(key):
-    return V[key]
+# set the starting vertices
+d[start_key_arr[0]] = 0
+d[start_key_arr[1]] = 0
 
-def d_get_all(coordinates):
-    ret = []
-    for DIR in [UP, DOWN, LEFT, RIGHT]:
-        for i in range(3):
-            dd = dget(get_key(coordinates, DIR, i))
-            if dd != float('inf'):
-                ret.append(dd)
-    return ret
-
-def v_get_all(coordinates):
-    ret = []
-    for DIR in [UP, DOWN, LEFT, RIGHT]:
-        for i in range(3):
-            dd = vget(get_key(coordinates, DIR, i))
-            if dd != False:
-                ret.append(dd)
-    return ret
-
-# set the starting vertex
-s = get_key((0, 0), RIGHT, 0)
-sp = get_key((0, 0), DOWN, 0)
-dput(s, 0)
-
-q = [(0,) + s, (0,) + sp]
+# init the heapq
+q = [(0,) + start_key_arr[0], (0,) + start_key_arr[1]]
 
 while q:
-    u = hq.heappop(q)
-    x, *xxxx = u
-    u = tuple(xxxx)
-    u_cords, u_direct, u_blinl = key_to_val(u)
 
-    if vget(u):
+    u = hq.heappop(q)
+    cost, *key = u
+    u = tuple(key)
+
+    if visited[u] is True:
         continue
 
     # for every neighbor of u
-    for v in [((u_cords[0] + DIRECTION_TO_CORDS_SHIFT[DIR][0], u_cords[1] + DIRECTION_TO_CORDS_SHIFT[DIR][1]), DIR, 1 if DIR != u_direct else u_blinl + 1) for DIR in [UP, DOWN, LEFT, RIGHT]]:
-        v_cords, v_direct, v_blinl = v
-        v = get_key(v_cords, v_direct, v_blinl)
+    for v in get_neighbors(u):
+        
+        # check if visited
+        if visited[v] is True:
+            continue
 
-        # check if is valid and not visited
-        if 0 <= v_cords[0] < len(input) and 0 <= v_cords[1] < len(input[0]) and vget(v) is False and v_blinl <= 3:
-            
-            # check if better way is possible, if yes then write new distance
-            if dget(v) > dget(u) + int(input[v_cords[0]][v_cords[1]]):
-                dput(v, dget(u) + int(input[v_cords[0]][v_cords[1]]))
-            
-            if ((dget(v),) + v) not in q:
-                hq.heappush(q, (dget(v),) + v)
+        if d[v] > d[u] + int(input[v[0]][v[1]]):
+            d[v] = d[u] + int(input[v[0]][v[1]])
+            p[v] = u
 
-    vput(u)
+        # push to queue
+        i = find_index_by_values_filter(q, v[:4])
+        if i == -1:
+            hq.heappush(q, (d[v],) + v)
 
-print(d_get_all((len(input)-1, len(input[0])-1)))
+    visited[u] = True
+
+print(min(get_ans()))
--- day17/puzzle2.py
+++ day17/puzzle2.py
@@ -5,5 +5,124 @@ inputf = open(os.path.dirname(__file__) + f"/input{n}.txt", "r")
 input = inputf.read().split("\n")
 inputf.close()
 # -------------------------------- PUZZLE CODE ------------------------------- #
+import heapq as hq
+from collections import defaultdict
 
+# length vector
+d = defaultdict(lambda : float('inf'))
+p = {}
 
+# visited vector
+visited = defaultdict(lambda : False)
+
+# DIRECTIONS ENUM
+UP = 1
+DOWN = 2
+RIGHT = 3
+LEFT = 4
+DIRECTION_TO_CORDS_SHIFT = {UP: (-1, 0), DOWN: (1, 0), RIGHT: (0, 1), LEFT: (0, -1)}
+BACK_STEP = {UP: DOWN, DOWN: UP, RIGHT: LEFT, LEFT: RIGHT}
+
+def get_neighbors(u):
+    for DIR in [UP, DOWN, RIGHT, LEFT]:
+        # If the neighbor is not a step back
+        if DIR != BACK_STEP[u[2]]:
+            shift = DIRECTION_TO_CORDS_SHIFT[DIR]
+            new_blocks_count = 1 if DIR != u[2] else u[3] + 1
+            cords_x = u[0] + shift[0]
+            cords_y = u[1] + shift[1]
+            if 0 <= cords_x < len(input) and 0 <= cords_y < len(input[0]) and 4 <= u[3] <= 10:
+                if cords_x != len(input)-1 or cords_y != len(input[0])-1:
+                    yield (cords_x, cords_y, DIR, new_blocks_count)
+                else:
+                    if new_blocks_count >= 4:
+                        yield (cords_x, cords_y, DIR, new_blocks_count)
+            elif 0 <= cords_x < len(input) and 0 <= cords_y < len(input[0]) and u[3] < 4 and DIR == u[2]:
+                if cords_x != len(input)-1 or cords_y != len(input[0])-1:
+                    yield (cords_x, cords_y, DIR, new_blocks_count)
+                else:
+                    if new_blocks_count == 4:
+                        yield (cords_x, cords_y, DIR, new_blocks_count)
+
+def find_index_by_values_filter(lst, target_values):
+    filtered_tuples = filter(lambda tpl: tpl[1:5] == target_values, lst)
+    try:
+        index = lst.index(next(filtered_tuples))
+    except StopIteration:
+        index = -1
+    return index
+
+def get_ans():
+    res = []
+    for i in range(1, 5):
+        for j in range(12):
+            ans = d[(len(input)-1, len(input[0])-1, i, j)]
+            if ans != float('inf'):
+                res.append(ans)
+    return res
+
+def print_path():
+
+    DIR_TO_CHAR = {UP: "^", DOWN: "v", RIGHT: ">", LEFT: "<"}
+
+    best = float('inf')
+    bc = None
+    for i in range(1, 5):
+        for j in range(4, 12):
+            ans = d[(len(input)-1, len(input[0])-1, i, j)]
+            if ans < best:
+                bc = (len(input)-1, len(input[0])-1, i, j)
+                best = ans
+    
+    node = bc
+    ic = input.copy()
+    ic = list(map(list, ic))
+    while True:
+        ic[node[0]][node[1]] = DIR_TO_CHAR[node[2]]
+        node = p[node]
+        if node[0] == 0 and node[1] == 0:
+            break
+    for row in ic:
+        for c in row:
+            print(c, end="")
+        print()
+
+# key struct
+# tuple(x coordinate, y coordinate, direction, blocks count)
+start_key_arr = [(0, 0, RIGHT, 0), (0, 0, DOWN, 0)]
+
+# set the starting vertices
+d[start_key_arr[0]] = 0
+d[start_key_arr[1]] = 0
+
+# init the heapq
+q = [(0,) + start_key_arr[0], (0,) + start_key_arr[1]]
+
+while q:
+
+    u = hq.heappop(q)
+    cost, *key = u
+    u = tuple(key)
+
+    if visited[u] is True:
+        continue
+
+    # for every neighbor of u
+    for v in get_neighbors(u):
+        
+        # check if visited
+        if visited[v] is True:
+            continue
+
+        if d[v] > d[u] + int(input[v[0]][v[1]]):
+            d[v] = d[u] + int(input[v[0]][v[1]])
+            p[v] = u
+
+        # push to queue
+        i = find_index_by_values_filter(q, v[:4])
+        if i == -1:
+            hq.heappush(q, (d[v],) + v)
+
+    visited[u] = True
+
+print(min(get_ans())+1)
